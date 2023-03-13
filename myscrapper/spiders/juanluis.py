ef5ec1ef126c625d62227b809411ee758d34dcef
MundakaZgz <j.herrero.moriana@gmail.com>
2023-03-13 23:33:27 +0100

    Modified spyder to return status and url

--- myscrapper/spiders/juanluis.py
+++ myscrapper/spiders/juanluis.py
@@ -8,13 +8,10 @@ class JuanluisSpider(CrawlSpider):
     allowed_domains = ["juanlu.is"]
     start_urls = ["https://juanlu.is/"]
 
-    rules = (Rule(LinkExtractor(allow=r"/"), callback="parse_item", follow=True),)
+    rules = (Rule(LinkExtractor(), callback="parse_item", follow=True),)
 
     def parse_item(self, response):
-        item = {}
-        #item["page"] = response.xpath('//h1[@id="page-title"]/a').get()
-        #item["domain_id"] = response.xpath('//input[@id="sid"]/@value').get()
-        #item["name"] = response.xpath('//h1[@id="page-title"]/a').get(),
-        #item["url"] = response.xpath('//h1[@id="page-title]/a::attr(href)').get()
-        #item["description"] = response.xpath('').get()
-        return item
+        yield {
+            "status": response.status,
+            "url": response.url
+        }
